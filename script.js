3aaf6f812409433471e95bb29872e6896bf2dec2
SnubStumpy2007 <joshua192007@gmail.com>
2024-04-09 17:08:11 -0400

    update

---
+++ script.js
@@ -0,0 +1,8 @@
+document.getElementById("btn").addEventListener("click", function() {
+    const inputText = document.getElementById("input").value
+    switch(inputText) {
+        case "Jude Mathis":
+            alert("Jude Mathis")
+            break;
+    }
+})
